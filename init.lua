10be67c0491ebf4e5aed6a0b6575374c844551b1
cherry <16375167+gIaceon@users.noreply.github.com>
2023-03-05 14:26:20 -0500

    Fix no default case error

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
--- TODO optimize, maybe make it just a single value for a case instead?
 export type SwitchAcceptedStatementType = any?;
 export type DefaultType = {};
 export type CaseType = ({[number]: SwitchAcceptedStatementType}) -> {[number]: SwitchAcceptedStatementType};
@@ -26,18 +25,22 @@ end;
 
 local mt = {
 	__index = {
-		Default = Default; Case = Case; DefaultEvaluateSwitchCase = DefaultEvaluateSwitchCase;	
-	}, 
-	__call = function(self, Passed_Argument: SwitchAcceptedStatementType?, EvalutateFunction: EvalutateFunctionType?)		
+		Default = Default;
+		Case = Case;
+		DefaultEvaluateSwitchCase = DefaultEvaluateSwitchCase;
+	},
+	__call = function(self, Passed_Argument: SwitchAcceptedStatementType?, EvalutateFunction: EvalutateFunctionType?)
 		return function (Passed_Table: PassedTable)
+			EvalutateFunction = EvalutateFunction or DefaultEvaluateSwitchCase;
 			for CaseOrDefault, CaseFunction in pairs(Passed_Table) do
-				if (CaseOrDefault == self.Default) then continue; end; -- Go to the next case if its default.
-				local Done, Equal = if EvalutateFunction ~= nil then EvalutateFunction(CaseOrDefault, Passed_Argument) else 
-					DefaultEvaluateSwitchCase(CaseOrDefault, Passed_Argument);
-
+				-- Skip default case
+				if (CaseOrDefault == self.Default) then continue; end;
+				local Done, Equal = EvalutateFunction(CaseOrDefault, Passed_Argument);
 				if (Done == true) then return CaseFunction(Equal, Passed_Argument); end;
 			end;
-
+			if (not Passed_Table[Default]) then
+				return;
+			end;
 			return (Passed_Table[Default])(Passed_Argument);
 		end;
 	end;
